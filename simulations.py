56650444fcd5cfd866898e031ab64e1b9f8bedf5
GabrielAureo <gabrielaureo@gmail.com>
2022-02-16 21:06:38 -0300

    md1 simulation and generalization of simulation method

--- simulations.py
+++ simulations.py
@@ -3,16 +3,74 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
+
+def m_queue(lamda, mu, max_time = 1000, max_events = 1000, kind = 'm'):
+  assert kind in ['m','d']
+  wait_function = lambda x: np.random.exponential(1/x) if kind == 'm' else 1/x
+  time = 0
+  nevents = 0
+  ledger = []
+  #fila de eventos, arrival @ t = 0
+  equeue = [[time, 'a']]
+
+  #variavel aleatória que representa o no de pessoas na fila
+  N = 0
+  while(nevents < max_events and time < max_time):
+    nevents += 1
+    event = equeue.pop(0)
+    time, etype = event
+
+    if(etype == 'a'):
+   
+      N += 1
+      if(N == 1):
+        service_time = time + wait_function(mu)
+        equeue.append([service_time, 's'])
+      equeue.append([time +  np.random.exponential(1/lamda), 'a'])
+      equeue = sorted(equeue, key = lambda x : x[0])
+    else:
+      N -= 1
+      if(N > 0):
+        equeue.append([time +   wait_function(mu), 's'])
+        equeue = sorted(equeue, key = lambda x : x[0])
+        
+    ledger.append([time, N, etype])
+
+  return ledger
+
+def queue_sim(lamda, mu, max_time = 1000, max_events = 1000, runs = 10, kind = 'm'):
+  ledger_df = []
+
+  for i in range(runs):
+      ledger = m_queue(lamda, mu, max_time = max_time, max_events = max_events, kind = kind)
+      ledger = pd.DataFrame(ledger, columns = ['time','N', 'op'])
+      ledger['run'] = i
+      ledger['holding'] = ledger.time.shift(-1) - ledger.time
+      ledger_df.append(ledger)
+
+  ledger_df = pd.concat(ledger_df)
+
+  ledger_df.dropna(how = 'any', axis = 0, inplace = True)
+  return ledger_df
+
+
+def utilization(ledger):
+  busy_time = ledger[ledger.N > 0].groupby('run').holding.sum()
+  total_time = ledger.groupby('run').holding.sum()
+  
+  utilization = busy_time/total_time
+
+  ci = confidence_interval(utilization)
+
+  return utilization.mean(), ci
+
 def mm1_queue(lamda, mu, max_time = 1000, max_events = 1000):
   time = 0
   nevents = 0
   ledger = []
-  waits = []
   #fila de eventos, arrival @ t = 0
   equeue = [[time, 'a']]
 
-  #fila FIFO real, recebe apenas arrivals e remove o primeiro item em um evento de serviços 
-  queue = [time]
   #variavel aleatória que representa o no de pessoas na fila
   N = 0
   while(nevents < max_events and time < max_time):
@@ -26,39 +84,81 @@ def mm1_queue(lamda, mu, max_time = 1000, max_events = 1000):
       if(N == 1):
         service_time = time + np.random.exponential(1/mu)
         equeue.append([service_time, 's'])
-      queue.append(time)
       equeue.append([time +  np.random.exponential(1/lamda), 'a'])
       equeue = sorted(equeue, key = lambda x : x[0])
     else:
       N -= 1
-      arrival_time = queue.pop(0)
-      waits.append(time - arrival_time)
       if(N > 0):
         equeue.append([time +  np.random.exponential(1/mu), 's'])
         equeue = sorted(equeue, key = lambda x : x[0])
         
     ledger.append([time, N, etype])
 
-  return ledger, waits
+  return ledger
 
+def md1_queue(lamda, mu, max_time = 1000, max_events = 1000):
+  time = 0
+  nevents = 0
+  ledger = []
+  #fila de eventos, arrival @ t = 0
+  equeue = [[time, 'a']]
 
-def mm1_simulation(lamda, mu, max_time = 1000, max_events = 1000, runs = 10):
+  N = 0
+  while(nevents < max_events and time < max_time):
+    
+    nevents += 1
+    event = equeue.pop(0)
+    time, etype = event
+
+    if(etype == 'a'):
+   
+      N += 1
+      if(N == 1):
+        service_time = time + 1/mu
+        equeue.append([service_time, 's'])
+      equeue.append([time +  np.random.exponential(1/lamda), 'a'])
+      equeue = sorted(equeue, key = lambda x : x[0])
+    else:
+      N -= 1
+      if(N > 0):
+        equeue.append([time + 1/mu, 's'])
+        equeue = sorted(equeue, key = lambda x : x[0])
+        
+    ledger.append([time, N, etype])
+  return ledger
+
+def md1_simulation(lamda, mu, max_time = 1000, max_events = 1000, runs = 10):
   ledger_df = []
-  waits_df = []
+
   for i in range(runs):
-      ledger, waits = mm1_queue(lamda, mu, max_time = max_time, max_events = max_events)
+      ledger = md1_queue(lamda, mu, max_time = max_time, max_events = max_events)
       ledger = pd.DataFrame(ledger, columns = ['time','N', 'op'])
-      waits = pd.DataFrame(waits, columns = ['wait'])
-      waits['run'] = i
       ledger['run'] = i
       ledger['holding'] = ledger.time.shift(-1) - ledger.time
       ledger_df.append(ledger)
-      waits_df.append(waits)
+
+  ledger_df = pd.concat(ledger_df)
+
+  ledger_df.dropna(how = 'any', axis = 0, inplace = True)
+  return ledger_df
+
+def mm1_simulation(lamda, mu, max_time = 1000, max_events = 1000, runs = 10):
+  ledger_df = []
+
+  for i in range(runs):
+      ledger = mm1_queue(lamda, mu, max_time = max_time, max_events = max_events)
+      ledger = pd.DataFrame(ledger, columns = ['time','N', 'op'])
+
+
+      ledger['run'] = i
+      ledger['holding'] = ledger.time.shift(-1) - ledger.time
+      ledger_df.append(ledger)
+
       
   ledger_df = pd.concat(ledger_df)
-  waits_df = pd.concat(waits_df)
+
   ledger_df.dropna(how = 'any', axis = 0, inplace = True)
-  return ledger_df, waits_df
+  return ledger_df
 
 
 def customers_mean(ledger):
